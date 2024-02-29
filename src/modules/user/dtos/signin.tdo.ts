de7f673d4c1a0abe0027a2eb90405a344d5bb637
eslam sabry <eslamsabry239@gmail.com>
2024-02-29 21:17:58 +0300

    add_signup_signIn_endpoints

---
+++ src/modules/user/dtos/signin.tdo.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+export class SignInDto {
+  email: string;
+  password: string;
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { User } from './user.model';
+import { SignUpDto } from './dtos/signup.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -21,5 +23,45 @@ export class UserService {
 
     return response;
   }
+
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = await User.findOne({
+        where: {
+          email: signUpDto.email,
+        },
+      });
+      if (user) throw new Error('User already exists');
+
+      const hashPassword = await bcrypt.hash(signUpDto.password, 5);
+
+      const newUser: User = await User.create({
+        ...signUpDto,
+        password: hashPassword,
+      } as unknown as User);
+      return newUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async signIn(signInDto: SignUpDto) {
+    try {
+      const user = await User.findOne({
+        where: {
+          email: signInDto.email,
+        },
+      });
+      if (!user) throw new Error('User not found');
+
+      const isMatch = await bcrypt.compare(signInDto.password, user.password);
+
+      if (isMatch && user.email === signInDto.email)
+        return 'Signed Up successfully';
+
+      throw new Error('Wrong password or email');
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
-//fasd
